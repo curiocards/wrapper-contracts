c8155745751e2a84360baac2cda3e70f3d28df60
Kian <fafrd@users.noreply.github.com>
2022-09-21 15:08:17 -0700

    todos

--- contracts/17bWrapper/Curio17bERC1155Wrapper.sol
+++ contracts/17bWrapper/Curio17bERC1155Wrapper.sol
@@ -5,7 +5,9 @@ import "./AbstractWrapper.sol";
 contract Curio17bERC1155Wrapper is AbstractWrapper {
 
     function initialize() internal {
-        create(172, 0xE0B5E6F32d657e0e18d4B3E801EBC76a5959e123, "ipfs://QmY2qTLrNmUvwVyKQF1JFqxSyZPtDH9cBo4CteLY3mtUYb"); // TODO replace with new image
+        // TODO change to 0xE0B5E6F32d657e0e18d4B3E801EBC76a5959e123
+        // TODO replace with new image
+        create(172, 0x6cdD2Af7A81A438E684B16d2f7E6881c1d99EB17, "ipfs://QmY2qTLrNmUvwVyKQF1JFqxSyZPtDH9cBo4CteLY3mtUYb");
     }
 
     constructor() AbstractWrapper() public {}
